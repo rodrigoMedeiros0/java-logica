c2675011c1954a672c145dbb97110d6518e2d178
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2024-04-08 20:45:28 -0300

    criando projeto do curso

---
+++ src/main/java/com/youtube/logica/EstruturaDeDecisao.java
@@ -0,0 +1,16 @@
+package com.youtube.logica;
+
+public class EstruturaDeDecisao {
+
+    public static void main(String[] args) {
+        Integer numero = 2;
+
+        if (numero % 2 == 0) {
+            //executo o codigo aqui
+            System.out.println("par");
+        } else {
+            //executo o codigo aqui quando falso
+            System.out.println("impar");
+        }
+    }
+}
