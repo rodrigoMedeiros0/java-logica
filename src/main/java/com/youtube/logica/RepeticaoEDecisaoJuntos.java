83f1808a3c2a9ee4528bb847e751b779bfff826c
Rodrigo Medeiros <rodrigovinicius.bsb@gmail.com>
2024-04-10 22:32:41 -0300

    estrutura de repeticao - exercicio final

--- src/main/java/com/youtube/logica/RepeticaoEDecisaoJuntos.java
+++ src/main/java/com/youtube/logica/RepeticaoEDecisaoJuntos.java
@@ -1,7 +1,31 @@
 package com.youtube.logica;
 
+import java.util.ArrayList;
+
 public class RepeticaoEDecisaoJuntos {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> listaDeUmADez = new ArrayList<>();
+
+        for (int posicao = 1; posicao <= 10; posicao++){
+            listaDeUmADez.add(posicao);
+        }
+
+        ArrayList<Integer> numerosPares = new ArrayList<>();
+        ArrayList<Integer> numerosImpares = new ArrayList<>();
+        for (Integer numero : listaDeUmADez) {
+            if (numero % 2 == 0) {
+                numerosPares.add(numero);
+            } else {
+                numerosImpares.add(numero);
+            }
+        }
+
+        for (Integer numeroPar : numerosPares) {
+            System.out.println(numeroPar);
+        }
+
+        for (Integer numeroImpar : numerosImpares) {
+            System.out.println(numeroImpar);
+        }
     }
 }
